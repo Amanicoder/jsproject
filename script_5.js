c6adb3c00674eeec583ddf7c8a2175ab3d188db8
amani <ameni.aiesec@gmail.com>
2020-05-22 03:52:03 +0100

    final

--- script_5.js
+++ script_5.js
@@ -1,66 +1,75 @@
-//Analyses de données Startup Nation
-const entrepreneurs = [
-  { first: 'Steve', last: 'Jobs', year: 1955 },
-  { first: 'Oprah', last: 'Winfrey', year: 1954 },
-  { first: 'Bill', last: 'Gates', year: 1955 },
-  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
-  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
-  { first: 'Beyonce', last: 'Knowles', year: 1981 },
-  { first: 'Jeff', last: 'Bezos', year: 1964 },
-  { first: 'Diane', last: 'Hendricks', year: 1947 },
-  { first: 'Elon', last: 'Musk', year: 1971 },
-  { first: 'Marissa', last: 'Mayer', year: 1975 },
-  { first: 'Walt', last: 'Disney', year: 1901 },
-  { first: 'Larry', last: 'Page', year: 1973 },
-  { first: 'Jack', last: 'Dorsey', year: 1976 },
-  { first: 'Evan', last: 'Spiegel', year: 1990 },
-  { first: 'Brian', last: 'Chesky', year: 1981 },
-  { first: 'Travis', last: 'Kalanick', year: 1976 },
-  { first: 'Marc', last: 'Andreessen', year: 1971 },
-  { first: 'Peter', last: 'Thiel', year: 1967 }
+//Analyses de données Bibliothécaire
+const books = [
+  { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
+  { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
+  { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
+  { title: 'Les frères Karamazov', id: 450911, rented: 55 },
+  { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
+  { title: 'Pourquoi j\'ai mangé mon père', id: 450911, rented: 45 },
+  { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
+  { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
+  { title: 'La disparition', id: 364445, rented: 33 },
+  { title: 'La lune seule le sait', id: 63541, rented: 43 },
+  { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
+  { title: 'Guerre et Paix', id: 748147, rented: 19 }
 ];
-/*console.log("Filtre dans cette liste les entrepreneurs qui sont nés dans les années 70");
- Here find function returns the value of  
- the first element in the array that satisfies  
- the provided testing function (return element > 4). 
-var age = entrepreneurs.filter(function(element) { 
-return element.year <= 1979 && element.year >=1970;  
-:}  );
-Printing desired values. 
-console.log(age); */
 
-//console.log(entrepreneurs[0].first);
 
-/*console.log("Sors une array qui contient le prénom et le nom des entrepreneurs");
-var entrepreneursfullname = new Array();
-for (var i =0; i< entrepreneurs.length; i++) {
-	entrepreneursfullname[i] = new Object();
-	entrepreneursfullname[i].first = entrepreneurs[i].first;
-	entrepreneursfullname[i].last = entrepreneurs[i].last;
+console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?");
+var rented_once = books.filter(function(element) { 
+return element.rented > 0 ;
+})
+//Printing desired values 
+if (rented_once.length=books.length) {
+  console.log("Tous les livres ont été empruntés au moins une fois");
 }
-console.log(entrepreneursfullname);*/
-
-/*console.log("Quel âge aurait chaque inventeur aujourd'hui ?");
-var entrepreneursage = new Array();
-for (var i =0; i< entrepreneurs.length; i++) {
-	entrepreneursage[i] = new Object();
-	entrepreneursage[i].first = entrepreneurs[i].first;
-	entrepreneursage[i].last = entrepreneurs[i].last;
-	entrepreneursage[i].age = 2020 - entrepreneurs[i].year;
+  else {
+    console.log("Au moins un livre n'a jamais été emprunté");
 }
-console.log(entrepreneursage);*/
 
-console.log("Trie les entrepreneurs par ordre alphabétique du nom de famille");
-var entrepreneursorder = new Array();
+console.log("Quel est livre le plus emprunté ?");
 
-	entrepreneursorder = entrepreneurs.sort(entrepreneurs.last);
+books.sort(function(a, b){
+ var rentedA=a.rented, rentedB=b.rented;
+ return rentedA - rentedB //sort string ascending
+});
+console.log(books[(books.length)-1]);
 
-entrepreneurs.sort(function(a, b){
- var lastA=a.last.toLowerCase(), lastB=b.last.toLowerCase();
- if (lastA < lastB) //sort string ascending
+
+console.log("Quel est livre le moins emprunté ?");
+
+books.sort(function(a, b){
+ var rentedA=a.rented, rentedB=b.rented;
+ return rentedA - rentedB //sort string ascending
+});
+console.log(books[0]);
+
+console.log("Trouve le livre avec lID: 873495");
+var book_tofind=books.filter(function(element) {
+ 
+return element.id == 873495;
+})
+//Printing desired values. 
+console.log(book_tofind);
+
+
+console.log("Supprime le livre avec l'ID: 133712");
+var books_new=books.filter(function(element) {
+ return (element.id !== 133712);
+})
+console.log(books_new);
+
+console.log("Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé)");
+//var bookssorder = new Array();
+
+  //bookssorder = books_new.sort(books_new.title);
+
+books_new.sort(function(a, b){
+ var titleA=a.title.toLowerCase(), titleB=b.title.toLowerCase();
+ if (titleA < titleB) //sort string ascending
   return -1;
- if (lastA > lastB)
+ if (titleA > titleB)
   return 1;
  return 0; //default return value (no sorting)
 });
-console.log(entrepreneurs);
+console.log(books_new);
